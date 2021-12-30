15f2c07870bc08a7be53790cb62eaed40e0785ad
paulwalk <paul@paulwalk.net>
2021-12-30 18:45:31 +0000

    turned website into interface

--- website_registry.go
+++ website_registry.go
@@ -1,43 +1,35 @@
 package SheepsTor_git
 
 type WebsiteRegistry struct {
-	WebSites []*Website
+	WebSites []*WebsiteInterface
 }
 
 var Registry WebsiteRegistry
 
 func InitialiseRegistry(sourceRoot, webRoot string) {
 	Registry = WebsiteRegistry{}
-	Registry.WebSites = make([]*Website, 0)
-	//for _, wc := range websiteConfigs {
-	//	if wc.Enabled {
-	//		logger.Debug(fmt.Sprintf("Configuring website '%s'", wc.ID))
-	//		w := NewWebsite(wc, sourceRootPath, webRoot)
-	//		r.WebSites = append(r.WebSites, &w)
-	//		logger.Info(fmt.Sprintf("Website '%s' configured OK", wc.ID))
-	//	} else {
-	//		logger.Warn(fmt.Sprintf("Configuration for website with ID '%s' is not enabled, so not loading this into registry", wc.ID))
-	//	}
-	//}
+	Registry.WebSites = make([]*WebsiteInterface, 0)
 }
 
-func (r *WebsiteRegistry) Add(w Website) {
+func (r *WebsiteRegistry) Add(w WebsiteInterface) {
 	r.WebSites = append(r.WebSites, &w)
 }
 
-func (r *WebsiteRegistry) GetWebsiteByRepoNameAndBranchRef(repoName, branchRef string) *Website {
-	for _, w := range r.WebSites {
-		if (w.GitRepo.RepoName == repoName) && (w.GitRepo.BranchRef == branchRef) {
-			return w
+func (r *WebsiteRegistry) GetWebsiteByRepoNameAndBranchRef(repoName, branchRef string) *WebsiteInterface {
+	for _, wptr := range r.WebSites {
+		w := *wptr
+		if w.HasRepoNameAndBranchRef(repoName, branchRef) {
+			return wptr
 		}
 	}
 	return nil
 }
 
-func (r *WebsiteRegistry) GetWebsiteByID(id string) *Website {
-	for _, w := range r.WebSites {
-		if w.ID == id {
-			return w
+func (r *WebsiteRegistry) GetWebsiteByID(id string) *WebsiteInterface {
+	for _, wptr := range r.WebSites {
+		w := *wptr
+		if w.HasID(id) {
+			return wptr
 		}
 	}
 	return nil
